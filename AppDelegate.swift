2c6b6d8f9ac0d223aca94507eaf1f7358d451e63
Annafi Islam <90231826+Paradox456@users.noreply.github.com>
2022-10-21 23:17:08 -0400

    Add files via upload

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -14,18 +14,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         let parseConfig = ParseClientConfiguration {
-                    $0.applicationId = "rw8O1SwCCymKjBybbpBlVBHfIaszEqjWxXUqeWF2" // <- UPDATE
-                    $0.clientKey = "sgUtfrSfjew6I4xqXEGfK7bMF3Znqld3RFM6QAhw" // <- UPDATE
-                    $0.server = "https://parseapi.back4app.com"
-            }
-            Parse.initialize(with: parseConfig)
+            $0.applicationId = "rw8O1SwCCymKjBybbpBlVBHfIaszEqjWxXUqeWF2"
+            $0.clientKey = "sgUtfrSfjew6I4xqXEGfK7bMF3Znqld3RFM6QAhw"
+            $0.server = "https://parseapi.back4app.com/"
+        }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
